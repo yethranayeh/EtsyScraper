16740df22618758e7467a7d4659a5ffa0dd2c4c5
yethranayeh <58709123+yethranayeh@users.noreply.github.com>
2021-08-21 14:12:56 +0300

    added scraping function

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,15 @@
-FROM python:3.8
+# Better build time
+FROM python:3.7-slim-stretch
 
-WORKDIR /scraper
+# Smaller image size
+# FROM python:3.7-alpine
+
+WORKDIR /app
 
 COPY requirements.txt .
 
-RUN pip3 install -r requirements.txt
+RUN pip3 install --user -r requirements.txt
 
 COPY . .
 
-# CMD [ "cd", "EtsyScraper", ";", "python3", "EtsyScraper/manage.py", "runserver", "0.0.0.0:8000" ]
 CMD cd EtsyScraper ; python3 manage.py runserver 0.0.0.0:8000
--- EtsyScraper/scraper/views.py
+++ EtsyScraper/scraper/views.py
@@ -1,10 +1,62 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
+from bs4 import BeautifulSoup
+import requests
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'scraper/index.html')
+    if request.method == "POST":
+        link = request.POST["etsyLink"]
+
+        page_content = requests.get(link).text
+
+        soup = BeautifulSoup(page_content, "html.parser")
+
+        try:
+            product_name = soup.find("h1", {"class":"wt-text-body-03 wt-line-height-tight wt-break-word wt-mb-xs-1"}).text.strip()
+        except:
+            product_name = soup.find("p", {"class":"nla-listing-title wt-text-body-03 wt-line-height-tight wt-mb-xs-1 wt-mb-sm-2"}).text.strip()
+
+        try:
+            img = soup.find("li", {"class":"wt-position-absolute wt-width-full wt-height-full wt-position-top wt-position-left carousel-pane", "data-index":"0"}).find("img").get("src")
+        except:
+            img = soup.find("div", {"class":"nla-listing-image wt-width-full wt-height-full wt-rounded-01"})
+            # print("!!!!!!!!!!!!!!!")
+            # print("CURRENT IMAGE:", img)
+            # print("!!!!!!!!!!!!!!!")
+            for element in str(img).split("'"):
+                print("ELEMENT:", element)
+                if "http" in element:
+                    img = element
+
+        try:
+            price = soup.find("p", {"class":"wt-text-title-03 wt-mr-xs-2"}).text.strip()
+        except:
+            price = soup.find("div", {"class":"wt-order-xs-1 wt-mb-xs-0 wt-mb-sm-3"}).text.strip()
+        price = price.split()[-1].replace("£", "").replace("+", "")
+
+        if product_name is not None:
+            if img is not None:
+                if price is not None:
+                    messages.add_message(request, messages.SUCCESS, f"Product: [ {product_name} ] is successfully added!")
+                else:
+                    messages.add_message(request, messages.ERROR, "There was a problem with retrieving product price")
+            else:
+                messages.add_message(request, messages.ERROR, "There was a problem with retrieving image link of the product")
+        else:
+            messages.add_message(request, messages.ERROR, "There was a problem with retrieving product name")
+        # print("-------")
+        # print(product_name)
+        # print("-------")
+        # print(img)
+        # print("-------")
+        # print(price)
+
+        return render(request, 'scraper/index.html')
+    else:
+        return render(request, 'scraper/index.html')
 
 def product(request):
     return render(request, "scraper/product.html")
